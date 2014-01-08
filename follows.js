e17d7529c24523b2ed5c88f1e02b491809090ef0
asavvin <a_savvin@hotmail.com>
2014-01-08 10:21:19 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= 'aUaqaEUyl74skKNXrLtZ9bQHfswtBjvyPWdYfizlB2lH3vsl', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [2033683223], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Andrey Savvin', // Replaces Xively logo in the header
-		dataDuration	= '1 day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '90-days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= 0A1922'', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
